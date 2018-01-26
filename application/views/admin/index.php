a39d48f284642efec165e6462aac9008489afee3
dxxue <running_ftd@163.com>
2018-01-26 18:56:18 +0800

    x

---
+++ application/views/admin/index.php
@@ -0,0 +1,21 @@
+<!--引入头部文件-->
+<?php include_once 'header.php';?>
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            <small>用户列表</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> 主页</a></li>
+            <li class="active">控制面板</li>
+        </ol>
+    </section>
+
+      <!--这里是每个页面的内容-->
+</div>
+
+<?php include_once "footer.php"; ?>
